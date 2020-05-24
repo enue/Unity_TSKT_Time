1dd485cf398b0c4c7f2d24467694f7c6038031db
enu <otoriden@gmail.com>
2020-05-24 09:34:13 +0900

    PlayTimeCounter

---
+++ Assets/Package/Runtime/PlayTimeCounter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TSKT
+{
+    public class PlayTimeCounter
+    {
+        readonly float initializedTime;
+        System.TimeSpan fixedDuration;
+
+        public PlayTimeCounter()
+        {
+            initializedTime = Time.unscaledTime;
+            fixedDuration = System.TimeSpan.Zero;
+        }
+
+        public PlayTimeCounter(System.TimeSpan duration)
+        {
+            initializedTime = Time.unscaledTime;
+            fixedDuration = duration;
+        }
+
+        public System.TimeSpan Duration
+        {
+            get => System.TimeSpan.FromSeconds(Time.unscaledTime - initializedTime) + fixedDuration;
+            set
+            {
+                fixedDuration = value - System.TimeSpan.FromSeconds(Time.unscaledTime - initializedTime);
+            }
+        }
+
+        public long ToTicks() => Duration.Ticks;
+        static public PlayTimeCounter FromTicks(long ticks)
+        {
+            return new PlayTimeCounter(new System.TimeSpan(ticks));
+        }
+    }
+}
